d0e3eb0c0b1b9d893fee8ecae282dbdf4bb5d47a
CarlosValera000 <166053458+CarlosValera000@users.noreply.github.com>
2025-02-05 15:00:57 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>CITADELLE DES MORTS é o pior mapa do BO6 e quem discorda está errado</h1>
+    return <h1>CITADELLE DES MORTS é o pior mapa do BO6 e quem discorda está errado!</h1>
 };
 
 export default Home;
