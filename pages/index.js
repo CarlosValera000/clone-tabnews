1a205ffa45b66fbc6cf92e5320773798920e0213
CarlosValera000 <166053458+CarlosValera000@users.noreply.github.com>
2025-02-09 23:55:40 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Dos mesmos criadores de: "Tranzit é o melhor mapa de zombies já criado". Vem aí: "Citadelle des Morts é o pior mapa de BO6". Discorda? Prove que eu estou errado.</h1>
-};
+  return (
+    <h1>
+      Dos mesmos criadores de: "Tranzit é o melhor mapa de zombies já criado".
+      Vem aí: "Citadelle des Morts é o pior mapa de BO6". Discorda? Prove que eu
+      estou errado.
+    </h1>
+  );
+}
 
 export default Home;
